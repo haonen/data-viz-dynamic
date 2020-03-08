d800077426c90867e92970e45845228d59eb7ecd
haonen <yuweiz0305@gmail.com>
2020-03-08 18:54:38 -0500

    add uid

---
+++ src/uid.js
@@ -0,0 +1,16 @@
+// example of how to export functions
+// this particular util only doubles a value so it shouldn't be too useful
+var count = 0;
+
+export default function uid(name) {
+  return new Id("O-" + (name == null ? "" : name + "-") + ++count);
+}
+
+function Id(id) {
+  this.id = id;
+  this.href = new URL(`#${id}`, location) + "";
+}
+
+Id.prototype.toString = function() {
+  return "url(" + this.href + ")";
+};
